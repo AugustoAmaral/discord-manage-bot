c7d2b01a0442150105ea19ddbf228ed41e384ae9
Augusto <guto_wow@hotmail.com>
2020-10-03 02:01:41 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const path = require("path");
+var filepath = path.resolve(process.cwd(), ".env");
+require("dotenv").config({ path: filepath });
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("pong");
+  }
+});
+
+client.login(process.env.BOT_KEY);
